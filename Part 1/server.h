5f4a0a2ce66c6cc460cab18c7a6349adae3337bd
Noabbo <58766968+Noabbo@users.noreply.github.com>
2019-12-27 23:54:46 +0200

    Add files via upload

--- Part 1/server.h
+++ Part 1/server.h
@@ -4,6 +4,7 @@
 
 #ifndef PART_1_SERVER_H
 #define PART_1_SERVER_H
+#include <algorithm>
 #include <thread>
 #include <string>
 #include <unordered_map>
@@ -28,36 +29,53 @@
 #include "Div.h"
 #include "Interpreter.h"
 #define OPEN_SERVER_COMMAND_RET_VALUE 2
+#define CONNECT_COMMAND_RET_VALUE 3
 using namespace std;
+vector<string> lexer(string file_name);
+vector<string> helpLexer(string line);
+string lowerCase(string s);
+string upperCase(string s);
+string removeSpaces(string str);
+void parser(vector<string> coms);
+void openServer(int port);
+// class of open server command
+class OpenServerCommand : public Command {
+public:
+    virtual int execute(vector<string> parameters);
+    virtual ~OpenServerCommand() {delete this;};
+
+};
+// class of define var command
+class DefineVarCommand : public Command {
+public:
+    virtual int execute(vector<string> parameters);
+    virtual ~DefineVarCommand() {delete this;};
+};
+
+class FuncCommand : public Command {
+    vector<string> commands;
+public:
+    virtual int execute(vector<string> parameters);
+    virtual ~FuncCommand() {delete this;};
+};
+
+class IfCommand : public Command {
+    vector<string> commands;
+public:
+    virtual int execute(vector<string> parameters);
+    virtual ~IfCommand() {delete this;};
+};
+
+class WhileCommand : public Command {
+    vector<string> commands;
+public:
+    virtual int execute(vector<string> parameters);
+    virtual ~WhileCommand() {delete this;};
+};
 unordered_map<string, Command*> commands;
 list<Variable> vars;
 unordered_map<string, typename list<Variable>::iterator> vars_map;
 unordered_map<string, typename list<Variable>::iterator> sim_map;
 unordered_map<string, FuncCommand> func_map;
 mutex mutex_lock;
-vector<string> lexer(string file_name);
-void openServer(int port);
-// class of open server command
-class OpenServerCommand : public Command {
-public:
-    virtual int execute(string parameters);
-
-};
-// class of define var command
-class DefineVarCommand : public Command {
-public:
-    virtual int execute(string parameters);
-};
-// class of set var command
-class SetVarCommand : public Command {
-    virtual int execute(string parameters);
-};
-
-class DefineFuncCommand : public Command {
-    virtual int execute(string parameters);
-};
-
-class FuncCommand : public Command {
-    virtual int execute(string parameters);
-};
 #endif //PART_1_SERVER_H
