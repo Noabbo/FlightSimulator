d49fd081569fa3d656e6858027d0f0359915c346
fguthmann <57538139+fguthmann@users.noreply.github.com>
2020-01-08 03:56:37 +0200

    Delete BinaryOperator.cpp

--- Part 1/BinaryOperator.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by noa on 19/11/2019.
-//
-#include "BinaryOperator.h"
-// constructor
-BinaryOperator::BinaryOperator(Expression *l, Expression *r) {
-    this->left = l;
-    this->right = r;
-}
